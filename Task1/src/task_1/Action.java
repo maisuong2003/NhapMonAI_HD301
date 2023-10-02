3080f49db2688b4292c0a37fa967b04c02a1ca12
maisuong2003 <suongmai.pc2003@gmail.com>
2023-10-02 17:56:22 +0700

    lab1

---
+++ Task1/src/task_1/Action.java
@@ -0,0 +1,5 @@
+package task_1; 
+
+public abstract class Action {
+	public abstract boolean isNoOp();
+}
---
+++ Task2/src/task_2/Action.java
@@ -0,0 +1,5 @@
+package task_2; 
+
+public abstract class Action {
+	public abstract boolean isNoOp();
+}
