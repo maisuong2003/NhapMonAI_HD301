0d027eb83d14f173d0bed4a0070b7257edf954d5
maisuong2003 <suongmai.pc2003@gmail.com>
2023-10-02 18:06:16 +0700

    Lab1

--- src/Task_1/NoOpAction.java
+++
@@ -1,9 +0,0 @@
-package Task_1; 
-
-public class NoOpAction extends Action {
-	public static final NoOpAction NO_OP = new NoOpAction();
-
-	public boolean isNoOp() {
-		return true;
-	}
-}
--- src/Task_2/Action.java
+++
@@ -1,5 +0,0 @@
-package Task_2; 
-
-public abstract class Action {
-	public abstract boolean isNoOp();
-}
