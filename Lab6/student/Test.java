17df5e1168cdf33d31679f33214c18f56905776d
maisuong2003 <146195289+maisuong2003@users.noreply.github.com>
2023-11-11 23:46:04 +0700

    21130516_Lab6

---
+++ Lab6/student/Test.java
@@ -0,0 +1,17 @@
+package student;
+
+public class Test {
+		public static void main(String[] args) {
+			Node initialState = new Node();
+
+			HillClimbingSearchQueen algo = new HillClimbingSearchQueen();
+			Node res = algo.executeWithRandomRestart(initialState);
+			System.out.println("Hill climbing:");
+			res.displayBoard();
+
+			SAAlgo_NQueen sAAlgo = new SAAlgo_NQueen();
+			Node result = sAAlgo.execute(initialState);
+			System.out.println("SA:");
+			result.displayBoard();
+	}
+}
