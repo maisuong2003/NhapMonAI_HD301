2bcf4b0c53a54cb4f87efe1fe4abc9237dfc17d8
maisuong2003 <146195289+maisuong2003@users.noreply.github.com>
2023-12-04 14:25:28 +0700

    21130516

---
+++ Lab9/game_nim_student/TestNode.java
@@ -0,0 +1,34 @@
+package game_nim_student;
+
+import java.util.Arrays;
+
+public class TestNode {
+
+	public static void main(String[] args) {
+		Node node = new Node();
+		MinimaxAlgo algo = new MinimaxAlgo();
+		
+		Integer[] data = { 7 };
+		node.addAll(Arrays.asList(data));
+		System.out.println("Numbers of tokens = 7");
+		System.out.print("Value = ");
+		algo.execute(node);
+		System.out.println("Best next move for MIN: " + node.getBestNextMove());
+		System.out.println("——————————————————————————————————");
+		
+		Integer[] data1 = { 9 };
+		node.addAll(Arrays.asList(data1));
+		System.out.println("Numbers of tokens = 9");
+		System.out.print("Value = ");
+		algo.execute(node);
+		System.out.println("Best next move for MIN: " + node.getBestNextMove());
+		System.out.println("——————————————————————————————————");
+
+		Integer[] data2 = { 5 };
+		node.addAll(Arrays.asList(data2));
+		System.out.println("Numbers of tokens = 5");
+		System.out.print("Value = ");
+		algo.execute(node);
+		System.out.println("Best next move for MIN: " + node.getBestNextMove());
+	}
+}
